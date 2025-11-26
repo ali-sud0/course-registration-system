627365195d3a054bca7e01de8a41c99882276e0f
unlineea <163650196+unlineea@users.noreply.github.com>
2025-11-27 00:31:05 +0330

    Cleanup: delete unused test scripts no longer required

--- backend/app/scripts/create_temp_hash.py
+++
@@ -1,6 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")  # or bcrypt if used
-
-hashed = pwd_context.hash("SuperSecret123")
-print(hashed)
--- backend/app/scripts/tempdb.py
+++
@@ -1,9 +0,0 @@
-# app/db.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-db_url = "mssql+pyodbc://sqluser:123456@localhost:1433/courseRegistrationSystem?driver=ODBC+Driver+17+for+SQL+Server"
-
-engine = create_engine(db_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
